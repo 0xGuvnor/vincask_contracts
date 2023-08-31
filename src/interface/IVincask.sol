2ad951e99e8488d146347a8b091440426b773c31
0xpaprhnds <yokeyeong@me.com>
2023-09-01 00:00:19 +0200

    Update IVincask.sol

--- src/interface/IVincask.sol
+++ src/interface/IVincask.sol
@@ -9,4 +9,30 @@ interface IVincask {
     error Vincask__CallerNotAuthorised();
     error Vincask__MustSetDifferentPrice();
     error Vincask__MustSetDifferentStableCoin();
+
+    function safeMultiMintWithStableCoin(uint256 _quantity) external;
+
+    function safeMultiMintWithCreditCard(uint256 _quantity, address _to) external;
+
+    function safeMultiMintAndBurnForAdmin(uint256 _quantity) external;
+
+    function multiRedeem(uint256[] calldata _tokenIds) external;
+
+    function multiApprove(uint256[] calldata _tokenIds) external;
+
+    function multiBurn(uint256[] calldata _tokenIds) external;
+
+    function setMintPrice(uint256 _newMintPrice) external;
+
+    function setStableCoin(address _newStableCoin) external;
+
+    function getTotalSupply() external view returns (uint256);
+
+    function getLatestTokenId() external view returns (uint256);
+
+    function getMintPrice() external view returns (uint256);
+
+    function getStableCoin() external view returns (address);
+
+    function getMultiSig() external view returns (address);
 }
